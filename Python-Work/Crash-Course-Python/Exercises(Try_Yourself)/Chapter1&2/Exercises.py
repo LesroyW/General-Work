c699957433768816baa2600662f4c1d4dc22fea6
wedderbl <wedderbl@aston.ac.uk>
2019-01-20 20:56:05 +0000

    Python work

---
+++ Python-Work/Crash-Course-Python/Exercises(Try_Yourself)/Chapter1&2/Exercises.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------------------------#
+#Strings
+message = "Hello new World would you like to learn some python today?"
+print(message)
+
+nameToChange = "Matt"
+
+#Title case
+print(nameToChange.title())
+#lowercase
+print(nameToChange.lower())
+#uppercase
+print(nameToChange.upper())
+
+print('Albert Einstein once said, "A person who never made a mistake never tired anything new"')
+
+famous_person = " Albert Einstein "
+quote = "A person who never made a mistake never tired anything new"
+print(famous_person + " " + quote)
+
+print(famous_person.strip())
+print(famous_person.lstrip())
+print(famous_person.rstrip())
+
+#-------------------------------------------------------------------------------------------------#
+#Numbers
+print(5 + 3)
+print(2 * 4)
+print(16 / 2)
+print(9 - 1)
+
+favourite_number = 8
+print("My favourite number is " + str(favourite_number))
+
+#-------------------------------------------------------------------------------------------------#
