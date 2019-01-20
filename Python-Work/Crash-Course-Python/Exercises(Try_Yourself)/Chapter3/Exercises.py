330e8ba0c96801708c593a910bcc6415c0e416db
wedderbl <wedderbl@aston.ac.uk>
2019-01-20 21:04:52 +0000

    Python work

---
+++ Python-Work/Crash-Course-Python/Exercises(Try_Yourself)/Chapter3/Exercises.py
@@ -0,0 +1,10 @@
+names = ["Simon", "Jeff", "Matt"]
+
+print("Hi my name is " + names[0])
+print("I'm " + names[1] + " nice to meet you")
+print("What can I do for you?, I'm " + names[2])
+
+peopleIwouldInvite = ["Fernando Alonso", "Barack Obama", "Lewis Hamilton"]
+print("Come to my party " + peopleIwouldInvite[-1])
+print("See you later, " + peopleIwouldInvite[0])
+print("Can wait to meet you " + peopleIwouldInvite[1])
