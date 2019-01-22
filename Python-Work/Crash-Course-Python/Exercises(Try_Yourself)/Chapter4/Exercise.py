2fc8c7f2ff74909b4b4d4d315874e0c4e708f520
wedderbl <wedderbl@aston.ac.uk>
2019-01-22 19:40:07 +0000

    Updated Exercises

--- Python-Work/Crash-Course-Python/Exercises(Try_Yourself)/Chapter4/Exercise.py
+++ Python-Work/Crash-Course-Python/Exercises(Try_Yourself)/Chapter4/Exercise.py
@@ -1,4 +1,7 @@
 pizzas = ["Pepperoni", "Cheese", "Ham"]
-
+tabs = "";
 for pizza in pizzas:
-    print("I love" + pizza + " pizza.")
+    print(tabs + "I love " + pizza + " pizza.")
+    tabs += "\t"
+
+print("I love pizza as it is great.\nMany combinations and tastes.\nYou should try it one day.\nI really love pizza!")
