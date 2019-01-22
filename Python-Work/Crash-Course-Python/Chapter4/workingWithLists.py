2215a6662f7b3ad2c51e2237755f297ec1a03d02
wedderbl <wedderbl@aston.ac.uk>
2019-01-22 19:36:27 +0000

    Updated Exercises

---
+++ Python-Work/Crash-Course-Python/Chapter4/workingWithLists.py
@@ -0,0 +1,12 @@
+magicians = ['alice', 'micheal', 'matt']
+#For loop to print out a list
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(magician.title() + ", is the greatest magician")
+    print(magician.title() + ", great things are to come. \n")
+
+print("Thank you everyone for attending")
+
+#Always indent when using a for loop in python. Indent out when finished using the loop
---
+++ Python-Work/Crash-Course-Python/Exercises(Try_Yourself)/Chapter4/Exercise.py
@@ -0,0 +1,4 @@
+pizzas = ["Pepperoni", "Cheese", "Ham"]
+
+for pizza in pizzas:
+    print("I love" + pizza + " pizza.")
