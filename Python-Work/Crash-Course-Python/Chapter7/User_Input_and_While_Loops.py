b5ff6ab442c94a953b12f0e0c025fd56414d7852
wedderbl <wedderbl@aston.ac.uk>
2019-02-09 22:52:27 +0000

    Beginning of exercises for chapter 7.

--- Python-Work/Crash-Course-Python/Chapter7/User_Input_and_While_Loops.py
+++ Python-Work/Crash-Course-Python/Chapter7/User_Input_and_While_Loops.py
@@ -6,14 +6,30 @@ print(message)
 #Input prompt should be clear and easy
 name = input("Please enter your name: ")
 print("Hello " + name + "!")
-
+#--------------------------------------------------------------------------------------------------------------------------#
 prompt = "We are first going to learn your name."
 prompt += "\nWhat is your name: "
 name = input(prompt)
 print("Hello " + name + "!")
-
+#--------------------------------------------------------------------------------------------------------------------------#
 #All inputs are interpreted as a string.
 prompt = "What is your age? "
 age = input(prompt)
-print( int(age) >= 20)
+print( int(age) >= 20) #Fixes the int input to string problem
+#--------------------------------------------------------------------------------------------------------------------------#
+#Using user input in if Statements
+prompt = "How old are you? "
+age = input(prompt)
+if int(age) > 20:
+    print("You can purchase the alcohol.")
+else:
+    print("You cannot purchase alcohol.")
+#--------------------------------------------------------------------------------------------------------------------------#
+#Modulo Operator
+prompt = "I can tell you a number is even or odd: "
+number = input(prompt)
+if int(number) % 2 == 0:
+    print(str(number) + " is even")
+else:
+    print(str(number) + " is odd")
 #--------------------------------------------------------------------------------------------------------------------------#
