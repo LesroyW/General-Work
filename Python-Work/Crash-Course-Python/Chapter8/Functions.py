9136621e0b3a91b57a0d7a25a5d88a0ee195e37e
wedderbl <wedderbl@aston.ac.uk>
2019-02-16 21:40:45 +0000

    Creation of chapter 8 folders & files

---
+++ Python-Work/Crash-Course-Python/Chapter8/Functions.py
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------------------------------------------------------------------#
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello! " + username.title())
+
+greet_user("mark")
+
+
+#positionalArguements
+def describe_pet(animal_type, pet_name):
+    print("\n I have a "+ animal_type + ".")
+    print("My " + animal_type + " is called " + pet_name.title())
+
+#can call function as many times as possible
+
+#--------------------------------------------------------------------------------------------------------------------------#
