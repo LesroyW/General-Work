9952df09751dc2893c7169dfddea4687ea070f66
wedderbl <wedderbl@aston.ac.uk>
2019-03-06 21:59:35 +0000

    Involvement of abstraction and Inheritence.

--- Python-Work/Crash-Course-Python/Chapter9/Classes.py
+++ Python-Work/Crash-Course-Python/Chapter9/Classes.py
@@ -56,6 +56,9 @@ class Car():
      def increment_odometer(self, miles):
          self.odometer_reading += miles
 
+     def fill_gas_tank():
+         print("Gas tank being filled!")
+
 my_new_car = Car("Ferrari", "Spider", 2009)
 print(my_new_car.describe_car())
 my_new_car.read_odometer()
@@ -65,5 +68,56 @@ my_new_car.update_odometer(-2)
 my_new_car.increment_odometer(400)
 my_new_car.read_odometer()
 
+#--------------------------------------------------------------------------------------------------------------------------#
+#Using Inheritence within python
+class Electriccar(Car):
+      def __init__(self, make,model,year):
+          super().__init__(make,model,year)
+          #Defining Attributes and methods for child class
+          self.battery_size = 90
 
+      def describe_battery(self):
+          print("This car has a " + str(self.battery_size) + " -kwh battery.")
+
+      #Overriding methods from the parent class
+      def fill_gas_tank(self):
+          print("This car doesn't need a gas tank!")
+
+
+
+my_new_car = Electriccar("Tesla", "Roadster", 2018)
+print(my_new_car.describe_car())
+my_new_car.describe_battery()
+my_new_car.fill_gas_tank()
+#--------------------------------------------------------------------------------------------------------------------------#
+#Instances as attributes
+class Battery():
+      def __init__(self, battery_size = 70):
+          self.battery_size = battery_size
+
+      def describe_battery(self):
+          print("The size of this battery is " + str(self.battery_size) + " -Kwh battery.")
+
+      def get_range(self):
+          if self.battery_size == 70:
+              range = 240
+          elif self.battery_size == 85:
+              range = 300
+
+          message = "This car has a range of: " + str(range) + " miles."
+          print(message)
+
+class ElectricCarv2(Car):
+      def __init__(self, make,model,year):
+          super().__init__(make,model,year)
+          #Defining Attributes and methods for child class
+          self.battery_size = Battery()
+
+      #Overriding methods from the parent class
+      def fill_gas_tank(self):
+          print("This car doesn't need a gas tank!")
+
+my_bmw = ElectricCarv2("BMW", "electricbwm", 2020)
+my_bmw.battery_size.describe_battery()
+my_bmw.battery_size.get_range()
 #--------------------------------------------------------------------------------------------------------------------------#
