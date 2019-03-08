975ebdfb898ee56de803b9b2eb233104dfa6b7eb
wedderbl <wedderbl@aston.ac.uk>
2019-03-08 23:19:23 +0000

    Added more examples to chapter 9

--- Python-Work/Crash-Course-Python/Chapter9/GenericCarClass.py
+++ Python-Work/Crash-Course-Python/Chapter9/GenericCarClass.py
@@ -5,6 +5,8 @@ my_tesla = car.ElectricCarv2("Tesla", "Model s", 2019)
 
 print(my_tesla.describe_car())
 print(my_bmw.describe_car())
+my_bmw.update_odometer(20)
+my_tesla.battery_size.get_range()
 
 
 #not recommended to import all classes from module. can cause confusion within names
