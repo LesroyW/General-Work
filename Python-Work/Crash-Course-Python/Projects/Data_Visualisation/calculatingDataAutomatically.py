01563cca25a0bab8a66863639796b09d849c31c9
Wlesroy98 <Wlesroy98@outlook.com>
2019-05-05 12:52:33 +0100

    added automatic data plotting

---
+++ Python-Work/Crash-Course-Python/Projects/Data_Visualisation/calculatingDataAutomatically.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+#Gets a list of x_values
+x_values = list(range(1,100))
+
+#Loops through x list
+y_values = [x**2 for x in x_values]
+
+#Produces a scatter graph
+plt.scatter(x_values, y_values, s=40)
+plt.title("Auto Squares")
+plt.xlabel("Value",fontsize=12)
+plt.ylabel("Squares of Values", fontsize=12)
+#Sets the range for each axis
+plt.axis([0,100, 0,10000])
+
+#Shows the graph
+plt.show()
