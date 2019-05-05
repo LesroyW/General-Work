1adb85450305df559972d6dc0d465e4063f44b46
Wlesroy98 <Wlesroy98@outlook.com>
2019-05-05 21:36:48 +0100

    Completed first section of Data visualisation

--- Python-Work/Crash-Course-Python/Exercises/Project-Chapter-DataVisualisation/Exercises.py
+++ Python-Work/Crash-Course-Python/Exercises/Project-Chapter-DataVisualisation/Exercises.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm
 
 x_values = list(range(5000))
 y_values = [x**3 for x in x_values]
-
-plt.scatter(x_values, y_values, s=12)
+cmap = matplotlib.cm.get_cmap('Reds')
+plt.scatter(x_values, y_values, c=y_values, cmap=cmap,s=12)
 plt.title("Cube Numbers")
 plt.xlabel("Values", fontsize =11)
 plt.ylabel("Squares of values", fontsize=11)
--- Python-Work/Crash-Course-Python/Projects/Data_Visualisation/calculatingDataAutomatically.py
+++ Python-Work/Crash-Course-Python/Projects/Data_Visualisation/calculatingDataAutomatically.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm 
+
 x_values = list(range(1001))
 y_values = [x**2 for x in x_values]
-plt.scatter(x_values, y_values,  s=40)
+cmap = matplotlib.cm.get_cmap('Reds')
+plt.scatter(x_values, y_values, c=y_values, cmap=cmap, edgecolor="none",s=40)
 plt.title("Auto Squares")
 plt.xlabel("Value",fontsize=12)
 plt.ylabel("Squares of Values", fontsize=12)
