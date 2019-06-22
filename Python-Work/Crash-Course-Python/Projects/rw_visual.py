e9fccc7c06c24076c1c39e17e9f311e8bf6e4d58
Wlesroy98 <Wlesroy98@outlook.com>
2019-06-22 23:25:47 +0100

    Python addition changing figure size

--- Python-Work/Crash-Course-Python/Projects/rw_visual.py
+++ Python-Work/Crash-Course-Python/Projects/rw_visual.py
@@ -5,11 +5,16 @@ while True:
 
     rw = RandomWalk()
     rw.fill_walk()
-
+    plt.figure(figsize=(10,6))
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_value, rw.y_value, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
-    plt.scatter(0,0, c='green', edgecolors='none', s=100)
+    plt.scatter(0,0, c='green', edgecolors='none', s=25)
     plt.scatter(rw.x_value[-1], rw.y_value[-1], c='red', edgecolor='none', s=15)
+    
+
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
