676da07368da0cf02cefad54946ccd84be5392a0
Wlesroy98 <Wlesroy98@outlook.com>
2019-07-22 20:28:32 +0100

    Creation of Dice Randomness

--- Python-Work/Crash-Course-Python/Projects/rw_visual.py
+++ Python-Work/Crash-Course-Python/Projects/rw_visual.py
@@ -10,11 +10,7 @@ while True:
     plt.scatter(rw.x_value, rw.y_value, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
     plt.scatter(0,0, c='green', edgecolors='none', s=25)
     plt.scatter(rw.x_value[-1], rw.y_value[-1], c='red', edgecolor='none', s=15)
-    
-
-    plt.axes().get_xaxis().set_visible(False)
-    plt.axes().get_yaxis().set_visible(False)
-    
+        
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
