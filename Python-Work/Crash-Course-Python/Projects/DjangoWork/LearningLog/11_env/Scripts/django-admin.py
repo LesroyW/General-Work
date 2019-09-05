aef3684c67ffee4af0e2882a5af0405df79044ff
Wlesroy98 <Wlesroy98@outlook.com>
2019-09-05 22:30:22 +0100

    Django Additions

---
+++ Python-Work/Crash-Course-Python/Projects/DjangoWork/LearningLog/11_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lesroy\documents\personalgitwork\general-work\python-work\crash-course-python\projects\djangowork\learninglog\11_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
