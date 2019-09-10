26c859b052dc3cfa2f8ae6ff778e7355948bc124
Wlesroy98 <Wlesroy98@outlook.com>
2019-09-10 21:44:39 +0100

    Update

--- Python-Work/Crash-Course-Python/Projects/DjangoWork/LearningLog/learning_logs/admin.py
+++ Python-Work/Crash-Course-Python/Projects/DjangoWork/LearningLog/learning_logs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from learning_logs.models import Topic
 # Register your models here.
+#Create models to show within the webpage
 admin.site.register(Topic)
 
 #page 440
