a42bca7f1646b06f325220ff038bf329a24e4b3c
Wlesroy98 <Wlesroy98@outlook.com>
2020-07-01 16:11:01 +0100

    Removed Previous project

--- Python-Work/Crash-Course-Python/Projects/DjangoWork/LearningLog/11_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lesroy\documents\personalgitwork\general-work\python-work\crash-course-python\projects\djangowork\learninglog\11_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- Python-Work/Crash-Course-Python/Projects/DjangoWork/LearningLog/learning_logs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from learning_logs.models import Topic, Entry
-# Register your models here.
-#Create models to show within the webpage
-admin.site.register(Topic)
-admin.site.register(Entry)
-
-#page 440
