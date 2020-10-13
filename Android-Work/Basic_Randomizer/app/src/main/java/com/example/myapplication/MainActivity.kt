52616593d97d17e098d7c6fc6264a383356e2237
LesroyW <wlesroy98@outlook.com>
2020-10-13 17:00:50 +0100

    Created randomizer using clickable button

---
+++ Android-Work/Basic_Randomizer/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonPressed(view: View) {
+          //random generator
+        val myRandomInt = Random.nextInt(100)
+        textView.text = "I have $myRandomInt friends."
+    }
+}
