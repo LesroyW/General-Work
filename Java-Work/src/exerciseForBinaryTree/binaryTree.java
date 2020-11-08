7513946f7c19172f30a3a2c05635bf2e12506888
LesroyW <wlesroy98@outlook.com>
2020-11-08 17:34:04 +0000

    Added info on deleting tree nodes/Searching

--- Java-Work/src/exerciseForBinaryTree/binaryTree.java
+++ Java-Work/src/exerciseForBinaryTree/binaryTree.java
@@ -61,6 +61,72 @@ public class binaryTree {
 		root = addRecursive(root, val);
 	}
 	
+	public Boolean containsRecursive(binaryTreeNode current, int val)
+	{
+		if(current == null)
+		{
+			return false;
+		}
+		
+		if(current.value == val)
+		{
+			return true;
+		}
+		
+		if(val < current.value)
+		{
+			return containsRecursive(current.left, val);
+		} else {
+			return containsRecursive(current.right, val);
+		}
+	}
+	
+	private binaryTreeNode deleteRecursive(binaryTreeNode current, int val)
+	{
+		if(current == null)
+		{
+			return null;
+		}
+		
+		if(current.value == val)
+		{
+			if(current.left == null && current.right == null)
+			{
+				return null;
+			}
+			
+			if(current.right == null)
+			{
+				return current.left;
+			}
+			
+			if(current.left == null)
+			{
+				return current.right;
+			}
+			
+			int smallestvalue = findSmallestValue(current.right);
+			current.value = smallestvalue;
+			current.right = deleteRecursive(current.right, smallestvalue);
+			return current;
+		}		
+		
+		
+		return val < current.value		
+		? deleteRecursive(current.left, val)
+		: deleteRecursive(current.right, val);
+		
+	}
+	
+	private int findSmallestValue(binaryTreeNode root) {
+		return root.left == null ? root.value : findSmallestValue(root.left);
+	}
+	
+	public void delete(int val)
+	{
+		root = deleteRecursive(root, val);
+	}
+	
 	public static void main(String[] args)
 	{
 		binaryTree bTree = new binaryTree();
@@ -69,11 +135,19 @@ public class binaryTree {
 		bTree.add(20);
 		bTree.add(7);
 		bTree.add(3);
+		bTree.add(29);
+		bTree.delete(10);
 		System.out.println("In Order:");
 		bTree.traverseInOrder(bTree.root);
 		System.out.println("pre Order:");
 		bTree.preOrder(bTree.root);
 		System.out.println("Post Order:");
 		bTree.postOrder(bTree.root);
+		if(bTree.containsRecursive(bTree.root, 1) == true)
+		{
+		System.out.println("Correct");
+		} else {
+			System.out.println("Does not exist");
+		}
 	}
 }
