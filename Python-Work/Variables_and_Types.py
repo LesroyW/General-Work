5765d1d275cef9b23f025c404ce5a970795b0548
LesroyW <wlesroy98@outlook.com>
2020-11-10 13:23:47 +0000

    restarting Python learning.

---
+++ Python-Work/Variables_and_Types.py
@@ -0,0 +1,7 @@
+#Numbers assignment is = 7, Similar with floats
+# Various way of accessing a string within python
+# Similar Assignments to Java with numbers etc
+#However assignments can be done on more than one variable "Simultaneously" for example
+number1, number2 = 6,7
+print(number1,number2)
+#Mixing numbers and strings is not viable
---
+++ Python-Work/lists.py
@@ -0,0 +1,7 @@
+#Lists similar to Arrays but can contain as many variables as you wish
+#Similar to arrays, Lists can be iterated voer
+
+#for x in mylist:
+    #print(x)
+
+#Can't access an index that doesn't exist
